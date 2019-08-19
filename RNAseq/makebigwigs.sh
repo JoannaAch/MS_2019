62d1afa92ac78b33a357ace9e36c9dfc8203cfcf
JoannaAch <JoannaAch@users.noreply.github.com>
2019-08-20 08:59:39 +1000

    Update makebigwigs.sh

--- RNAseq/makebigwigs.sh
+++ RNAseq/makebigwigs.sh
@@ -1,3 +1,117 @@
-qsub -cwd -N TAMR_bigwigs -pe smp 4 -l h_vmem=12G -b y -j y  ./makewigs_TAMR.sh
-qsub -cwd -N MCF7_bigwigs -pe smp 4 -l h_vmem=12G -b y -j y  ./makewigs_MCF7.sh
-qsub -cwd -N FASR_bigwigs -pe smp 4 -l h_vmem=12G -b y -j y  ./makewigs_FASR.sh
+#!/bin/bash -e
+
+source /etc/profile.d/modules.sh
+export MODULEPATH=/share/ClusterShare/Modules/modulefiles/noarch:/share/ClusterShare/Modules/modulefiles/centos6.2_x86_64:/share/ClusterShare/Modules/modulefiles/contrib:$MODULEPATH
+
+module load gi/bedtools/2.22.0 gi/ucsc_utils/283
+
+INPUT_FOLDER="FASR/"
+BAM_FILES=$(find $INPUT_FOLDER -name \*.bam)
+
+for bam_file in ${BAM_FILES[*]};
+do
+
+  # parse current file location into array
+  IFS='/' read -a array <<< "$bam_file"
+  # sample name will be the last index of array length
+  BAM_FILE_NAME=${array[${#array[@]}-1]}
+  
+  # remove "stats" to get bam file name
+  SAMPLE_NAME="$(sed 's/.genome.sorted.bam//g' <<< "$BAM_FILE_NAME")"
+  
+  CMD="genomeCoverageBed -bg -split -ibam ${bam_file} -g hg38.chrom.sizes > ${INPUT_FOLDER}${SAMPLE_NAME}.bedGraph"
+  echo $CMD & eval $CMD
+  
+  CMD="sed -n '/chr/p' ${INPUT_FOLDER}${SAMPLE_NAME}.bedGraph > ${INPUT_FOLDER}${SAMPLE_NAME}.bedGraph.filt"
+  echo $CMD & eval $CMD
+  
+  CMD="sort -k1,1 -k2,2n ${INPUT_FOLDER}${SAMPLE_NAME}.bedGraph.filt > ${INPUT_FOLDER}${SAMPLE_NAME}.bedGraph.filt.sorted"
+  echo $CMD & eval $CMD
+
+  CMD="bedGraphToBigWig ${INPUT_FOLDER}${SAMPLE_NAME}.bedGraph.filt.sorted hg38.chrom.sizes ${INPUT_FOLDER}${SAMPLE_NAME}.bw"
+  echo $CMD & eval $CMD
+  
+  CMD="rm ${INPUT_FOLDER}${SAMPLE_NAME}.bedGraph*"
+  echo $CMD & eval $CMD
+  
+done
+
+
+#!/bin/bash -e
+
+source /etc/profile.d/modules.sh
+export MODULEPATH=/share/ClusterShare/Modules/modulefiles/noarch:/share/ClusterShare/Modules/modulefiles/centos6.2_x86_64:/share/ClusterShare/Modules/modulefiles/contrib:$MODULEPATH
+
+module load gi/bedtools/2.22.0 gi/ucsc_utils/283
+
+INPUT_FOLDER="MCF7/"
+BAM_FILES=$(find $INPUT_FOLDER -name \*.bam)
+
+for bam_file in ${BAM_FILES[*]};
+do
+
+  # parse current file location into array
+  IFS='/' read -a array <<< "$bam_file"
+  # sample name will be the last index of array length
+  BAM_FILE_NAME=${array[${#array[@]}-1]}
+  
+  # remove "stats" to get bam file name
+  SAMPLE_NAME="$(sed 's/.genome.sorted.bam//g' <<< "$BAM_FILE_NAME")"
+  
+  CMD="genomeCoverageBed -bg -split -ibam ${bam_file} -g hg38.chrom.sizes > ${INPUT_FOLDER}${SAMPLE_NAME}.bedGraph"
+  echo $CMD & eval $CMD
+  
+  CMD="sed -n '/chr/p' ${INPUT_FOLDER}${SAMPLE_NAME}.bedGraph > ${INPUT_FOLDER}${SAMPLE_NAME}.bedGraph.filt"
+  echo $CMD & eval $CMD
+  
+  CMD="sort -k1,1 -k2,2n ${INPUT_FOLDER}${SAMPLE_NAME}.bedGraph.filt > ${INPUT_FOLDER}${SAMPLE_NAME}.bedGraph.filt.sorted"
+  echo $CMD & eval $CMD
+
+  CMD="bedGraphToBigWig ${INPUT_FOLDER}${SAMPLE_NAME}.bedGraph.filt.sorted hg38.chrom.sizes ${INPUT_FOLDER}${SAMPLE_NAME}.bw"
+  echo $CMD & eval $CMD
+  
+  CMD="rm ${INPUT_FOLDER}${SAMPLE_NAME}.bedGraph*"
+  echo $CMD & eval $CMD
+  
+done
+
+
+
+
+#!/bin/bash -e
+
+source /etc/profile.d/modules.sh
+export MODULEPATH=/share/ClusterShare/Modules/modulefiles/noarch:/share/ClusterShare/Modules/modulefiles/centos6.2_x86_64:/share/ClusterShare/Modules/modulefiles/contrib:$MODULEPATH
+
+module load gi/bedtools/2.22.0 gi/ucsc_utils/283
+
+INPUT_FOLDER="TAMR/"
+BAM_FILES=$(find $INPUT_FOLDER -name \*.bam)
+
+for bam_file in ${BAM_FILES[*]};
+do
+
+  # parse current file location into array
+  IFS='/' read -a array <<< "$bam_file"
+  # sample name will be the last index of array length
+  BAM_FILE_NAME=${array[${#array[@]}-1]}
+  
+  # remove "stats" to get bam file name
+  SAMPLE_NAME="$(sed 's/.genome.sorted.bam//g' <<< "$BAM_FILE_NAME")"
+  
+  CMD="genomeCoverageBed -bg -split -ibam ${bam_file} -g hg38.chrom.sizes > ${INPUT_FOLDER}${SAMPLE_NAME}.bedGraph"
+  echo $CMD & eval $CMD
+  
+  CMD="sed -n '/chr/p' ${INPUT_FOLDER}${SAMPLE_NAME}.bedGraph > ${INPUT_FOLDER}${SAMPLE_NAME}.bedGraph.filt"
+  echo $CMD & eval $CMD
+  
+  CMD="sort -k1,1 -k2,2n ${INPUT_FOLDER}${SAMPLE_NAME}.bedGraph.filt > ${INPUT_FOLDER}${SAMPLE_NAME}.bedGraph.filt.sorted"
+  echo $CMD & eval $CMD
+
+  CMD="bedGraphToBigWig ${INPUT_FOLDER}${SAMPLE_NAME}.bedGraph.filt.sorted hg38.chrom.sizes ${INPUT_FOLDER}${SAMPLE_NAME}.bw"
+  echo $CMD & eval $CMD
+  
+  CMD="rm ${INPUT_FOLDER}${SAMPLE_NAME}.bedGraph*"
+  echo $CMD & eval $CMD
+  
+done
