7bbfb5a2e2bfdef0967660253725e6a3c0a15e38
JoannaAch <JoannaAch@users.noreply.github.com>
2019-08-20 08:40:47 +1000

    Update bwa-mem.sh

--- bwa-mem.sh
+++ bwa-mem.sh
@@ -1,7 +1,14 @@
+#MCF7
+bwa mem -t 8 /share/ClusterShare/biodata/contrib/weejar/Homo_sapiens_assembly38_chromosome.fasta LizCaldon_MCF7C_WGS_R1.fastq.gz \
+LizCaldon_MCF7C_WGS_R2.fastq.gz -M \ -R '@RG\tID:LizCaldon_MCF7C_WGS\tPL:ILLUMINA\tPU:None\tLB:1\tSM:1\tCN:hcpcg' | samblaster -M --addMateTags | samtools view -h -S -b - \
+> LizCaldon_MCF7C_WGS.bam
+
+#TAMR
 bwa mem -t 8 /share/ClusterShare/biodata/contrib/weejar/Homo_sapiens_assembly38_chromosome.fasta LizCaldon_TAMR_WGS_R1.fastq.gz \
 LizCaldon_TAMR_WGS_R2.fastq.gz -M \ -R '@RG\tID:LizCaldon_TAMR_WGS\tPL:ILLUMINA\tPU:None\tLB:1\tSM:1\tCN:hcpcg' | samblaster -M --addMateTags | samtools view -h -S -b - \
 > LizCaldon_TAMR_WGS.bam
 
+#FASR
 bwa mem -t 8 /share/ClusterShare/biodata/contrib/weejar/Homo_sapiens_assembly38_chromosome.fasta LizCaldon_FASR_WGS_R1.fastq.gz \
 LizCaldon_FASR_WGS_R2.fastq.gz -M \ -R '@RG\tID:LizCaldon_FASR_WGS\tPL:ILLUMINA\tPU:None\tLB:1\tSM:1\tCN:hcpcg' | samblaster -M --addMateTags | samtools view -h -S -b - \
 > LizCaldon_FASR_WGS.bam
