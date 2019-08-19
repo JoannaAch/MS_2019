dfe706f6e4dc90bb8447b5cb877cbfd3b24d9e27
JoannaAch <JoannaAch@users.noreply.github.com>
2019-08-20 08:39:47 +1000

    Update MuTect2.sh

--- MuTect2.sh
+++ MuTect2.sh
@@ -1,3 +1,4 @@
+#FASR
 java -jar GenomeAnalysisTK.jar \
      -T MuTect2 \
      -R /share/ClusterShare/biodata/contrib/genomeIndices_garvan/iGenomes/Homo_sapiens/UCSC/hg38/Sequence/WholeGenomeFasta/genome.fa \
@@ -9,7 +10,7 @@ java -jar GenomeAnalysisTK.jar \
      --cosmic /share/ScratchGeneral/joaach/temp/CosmicNonCodingVariants.hg38.final2.vcf \
      -L chr1 -L chr2 -L chr3 -L chr4 -L chr5 -L chr6 -L chr7 -L chr8 -L chr9 -L chr10 -L chr11 -L chr12 -L chr13 -L chr14 -L chr15 -L chr16 -L chr17 -L chr18 -L chr19 -L chr20 -L chr21 -L chr22 -L chrX -L chrY
 
-
+#TAMR
 java -jar GenomeAnalysisTK.jar \
      -T MuTect2 \
      -R /share/ClusterShare/biodata/contrib/genomeIndices_garvan/iGenomes/Homo_sapiens/UCSC/hg38/Sequence/WholeGenomeFasta/genome.fa \
